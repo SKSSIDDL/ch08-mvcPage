027cb0383dfa119d2cbfb74e6d1fcef0a7fd161e
user <user@DESKTOP-COJ9858>
2023-11-23 17:44:25 +0900

    11.23

---
+++ src/main/java/kr/member/dao/MemberDAO.java
@@ -0,0 +1,121 @@
+package kr.member.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.member.vo.MemberVO;
+import kr.util.DBUtil;
+
+public class MemberDAO {
+	//싱글턴 패턴
+	private static MemberDAO instance = new MemberDAO();
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	private MemberDAO() {}
+	
+	//회원가입
+	public void insertMember(MemberVO member)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		PreparedStatement pstmt3 = null;
+		ResultSet rs = null;
+		String sql = null;
+		int num = 0; //시퀀스 번호를 저장
+		
+		try {
+			//커넥션풀로부터 커넥션 할당
+			conn = DBUtil.getConnection();
+			//오토커밋 해제
+			conn.setAutoCommit(false);
+			//회원 번호(mem_num) 생성
+			sql ="SELECT zmember_seq.nextval FROM dual";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				num = rs.getInt(1);
+			}
+			
+			//zmember 테이블에 데이터를 저장
+			sql = "INSERT INTO zmember (mem_num,id) VALUES (?,?)";
+			pstmt2 = conn.prepareStatement(sql);
+			pstmt2.setInt(1, num); //회원번호
+			pstmt2.setString(2,	member.getId()); //아이디
+			pstmt2.executeUpdate();
+			
+			//zmember_detail 테이블에 데이터를 저장
+			sql = "INSERT INTO zmember_detail (mem_num,name,passwd,phone,email,zipcode,address1,address2) "
+					+ "VALUES (?,?,?,?,?,?,?,?)";
+			pstmt3 = conn.prepareStatement(sql);
+			pstmt3.setInt(1, num);
+			pstmt3.setString(2, member.getName());
+			pstmt3.setString(3, member.getPasswd());
+			pstmt3.setString(4, member.getPhone());
+			pstmt3.setString(5, member.getEmail());
+			pstmt3.setString(6, member.getZipcode());
+			pstmt3.setString(7, member.getAddress1());
+			pstmt3.setString(8, member.getAddress2());
+			pstmt3.executeUpdate();
+			
+			//SQL문 실행시 모두 성공하면 COMMIT
+			conn.commit();
+			
+		}catch(Exception e) {
+			//SQL문이 하나라도 실패하면 ROLLBACK
+			conn.rollback();
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt3, null);
+			DBUtil.executeClose(null, pstmt2, null);
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+	}
+	
+	//아이디 중복체크 및 로그인 처리
+	public MemberVO checkMember(String id)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs= null;
+		MemberVO member = null;
+		String sql = null;
+		
+		try{
+			conn = DBUtil.getConnection();
+			//zmember와 zmember_detail 조인시 zmember의 누락된 데이터가 보여야 id 중복체크가 가능(=>left outer join 사용)
+			sql = "SELECT * FROM zmember LEFT OUTER JOIN zmember_detail USING(mem_num) WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				member = new MemberVO();
+				member.setMem_num(rs.getInt("mem_num"));
+				member.setId(rs.getString("id"));
+				member.setAuth(rs.getInt("auth"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setPhoto(rs.getString("photo"));
+			}
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return member;
+	}
+	//회원 상세 정보
+	//회원 정보 수정
+	//비밀번호 수정
+	//프로필 사진 수정
+	//회원 탈퇴(회원정보 삭제)
+	
+	
+	
+	//관리자
+	//전체 내용 개수, 검색 내용 개수
+	//목록, 검색 목록
+	//회원 등급 수정
+	
+}
