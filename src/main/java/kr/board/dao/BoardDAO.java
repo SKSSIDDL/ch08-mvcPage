1bab146a24443cb58ea7a265cef629f60e7f3ec8
user <user@DESKTOP-COJ9858>
2023-11-29 15:10:59 +0900

    11.29

--- src/main/java/kr/board/dao/BoardDAO.java
+++ src/main/java/kr/board/dao/BoardDAO.java
@@ -3,9 +3,12 @@ package kr.board.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.board.vo.BoardVO;
 import kr.util.DBUtil;
+import kr.util.StringUtil;
 
 public class BoardDAO {
 	//싱글턴 패턴
@@ -62,23 +65,141 @@ public class BoardDAO {
 				}
 			}
 			sql = "SELECT COUNT(*) FROM zboard JOIN zmember USING(mem_num) " + sub_sql;
-			pstmt = conn.prepareStatement(sub_sql);
+			pstmt = conn.prepareStatement(sql);
+			if(keyword != null && !"".equals(keyword)) {
+				pstmt.setString(1, "%"+keyword+"%");
+			}
+			//SQL문 실행
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
 			
 		}catch(Exception e) {
 			throw new Exception(e);
 		}finally {
 			DBUtil.executeClose(rs, pstmt, conn);
 		}
-		
-		
 		return count;
 	}
 	
 	//전체 글/검색 글 목록
-	//글 상세
-	//조회수 증가
+	public List<BoardVO> getlistBoard(int start, int end, String keyfield, String keyword)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVO> list = null;
+		String sql = null;
+		String sub_sql = "";
+		int cnt = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			
+			if(keyword!=null && !"".equals(keyword)) {
+				//검색처리
+				if(keyfield.equals("1")) {
+					sub_sql += "WHERE title LIKE ?";
+				}else if(keyfield.equals("2")) {
+					sub_sql += "WHERE id LIKE ?";
+				}else if(keyfield.equals("3")) {
+					sub_sql += "WHERE content LIKE ?";
+				}
+			}
+			
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM zboard JOIN zmember USING(mem_num) "
+				   + sub_sql + " ORDER BY board_num DESC)a) WHERE rnum>=? AND rnum<=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			if(keyword != null && !"".equals(keyword)) {
+				pstmt.setString(++cnt, "%"+keyword+"%");
+			}
+			pstmt.setInt(++cnt, start);
+			pstmt.setInt(++cnt, end);
+			
+			rs = pstmt.executeQuery();
+			list = new ArrayList<BoardVO>();
+			while(rs.next()) {
+				BoardVO board = new BoardVO();
+				board.setBoard_num(rs.getInt("board_num"));
+				//HTML을 허용하지 않음
+				board.setTitle(StringUtil.useNoHtml(rs.getString("title")));
+				board.setHit(rs.getInt("hit"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setId(rs.getString("id"));
+				
+				list.add(board);
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return list;
+	}
+	
+	//글 상세 - 조회수 증가랑 연관有
+	public BoardVO getBoard(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		BoardVO board = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			//sql문 작성 주의 (회원탈퇴하면 zmember_detail의 레코드가 존재하지 않기 때문에 외부 join을 사용해서 데이터 누락 방지)
+			sql = "SELECT * FROM zboard JOIN zmember USING(mem_num) "
+					+ "LEFT OUTER JOIN zmember_detail USING(mem_num) WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				board = new BoardVO();
+				board.setBoard_num(rs.getInt("board_num"));
+				board.setTitle(rs.getString("title"));
+				board.setContent(rs.getString("content"));
+				board.setHit(rs.getInt("hit"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setModify_date(rs.getDate("modify_date"));
+				board.setFilename(rs.getString("filename"));
+				board.setMem_num(rs.getInt("mem_num"));
+				board.setId(rs.getString("id"));
+				board.setPhoto(rs.getString("photo"));
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return board;
+	}
+	
+	//조회수 증가(detail로 들어가면 무조건 조회수 1 증가)
+	public void updateReadCount(int board_num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "UPDATE zboard SET hit=hit+1 WHERE board_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, board_num);
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
+	
 	//파일 삭제
 	//글 수정
+	
+	
 	//글 삭제
 	
 	//좋아요 등록
